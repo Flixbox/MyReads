dc718919f2297e4f34c0f8f75439ae808871cb15
Tietjen, Felix <Felix.Tietjen@txs.de>
2019-04-08 16:44:05 +0200

    feat: added theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+
+import { Navigation } from './Navigation'
+import { MainView } from './MainView'
+
+import theme from '../theme.js'
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <header>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
-    )
-  }
+    render() {
+        return (
+        <MuiThemeProvider theme={theme}>
+            <Navigation/>
+            <MainView/>
+        </MuiThemeProvider>
+        )
+    }
 }
 
 export default App
---
+++ src/theme.js
@@ -0,0 +1,10 @@
+import { createMuiTheme } from '@material-ui/core/styles'
+import purple from '@material-ui/core/colors/purple'
+
+export default createMuiTheme({
+    palette: {
+        primary: { main: purple[500] }, // Purple and green play nicely together.
+        secondary: { main: '#11cb5f' }, // This is just green.A700 as hex.
+    },
+    typography: { useNextVariants: true },
+})
