c935efdee00c4278212b84a753645ee40349db57
Tietjen, Felix <Felix.Tietjen@txs.de>
2019-04-09 09:05:21 +0200

    Updates

---
+++ precache-manifest.6ee6470932f637732642abe5634781b6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c45681028c3d486afd70",
+    "url": "/MyReads/static/js/runtime~main.72789c0d.js"
+  },
+  {
+    "revision": "b55e83a07888fab3225d",
+    "url": "/MyReads/static/js/main.6deb10ab.chunk.js"
+  },
+  {
+    "revision": "d0e0cf9f99a576a83413",
+    "url": "/MyReads/static/js/2.f939bfb1.chunk.js"
+  },
+  {
+    "revision": "8424ae581d3ea0d04cd977973be60ad6",
+    "url": "/MyReads/index.html"
+  }
+];
