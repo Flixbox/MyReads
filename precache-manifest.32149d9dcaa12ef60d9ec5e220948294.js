358d70ddbaa1501197a41f35e00e4109d961c5d9
Tietjen, Felix <Felix.Tietjen@txs.de>
2019-04-10 14:37:26 +0200

    Updates

--- precache-manifest.32149d9dcaa12ef60d9ec5e220948294.js
+++ precache-manifest.32149d9dcaa12ef60d9ec5e220948294.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/MyReads/static/js/runtime~main.72789c0d.js"
   },
   {
-    "revision": "5a9d62bae5353a1ea42e",
-    "url": "/MyReads/static/js/main.d80bf300.chunk.js"
+    "revision": "433d235d676d811a3ae5",
+    "url": "/MyReads/static/js/main.11da065d.chunk.js"
   },
   {
     "revision": "ac5db17d5745c2bc8623",
     "url": "/MyReads/static/js/2.3c7d8db5.chunk.js"
   },
   {
-    "revision": "31470bbebf43579fc1b9c2997923f793",
+    "revision": "3c0901215e0c2433b4944df8c7037437",
     "url": "/MyReads/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MyReads/precache-manifest.2afb67f9e465d95d28c044280dc9460d.js"
+  "/MyReads/precache-manifest.32149d9dcaa12ef60d9ec5e220948294.js"
 );
 
 workbox.clientsClaim();
